fb0c7941da6a28f15f9a5af9e212788881d4683a
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-11 11:55:44 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,20 +1,29 @@
+
+   require "pry"
 def square_array(array)
+  array.map {|element|  element**2}
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
 end
 
 def summon_captain_planet(planeteer_calls)
+ planeteer_calls.map {|element|  "#{element.capitalize}!"}
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
 end
 
 def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any?{ |element| planeteer_calls.length > 4 }
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
   # Return the boolean result of this check
 end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
+#binding.pry
+planeteer_calls.find {|planeteer_calls|  valid_calls.include?(planeteer_calls) }
+end
+  
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
-end
+
